8f6dea1b206971a7f0816ee5283d65ff743f4da6
Абдуллина Язиля <A_250494@mail.ru>
2022-07-24 15:30:24 +0300

    Зависимости junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
@@ -10,13 +10,13 @@ public class CashbackHackServiceTest {
     public void shouldReturnNull() {
         int actual = service.remain(1000);
         int expected = 0;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected,actual);
     }
 
     @Test
     public void shouldReturn100() {
         int actual = service.remain(900);
         int expected = 100;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected,actual);
     }
 }
